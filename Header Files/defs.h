f083435f1c6d9175dcaafb0719419da2530caa1c
tsila.allaoui <tsilavo.maminiaina@futurmap.com>
2023-02-23 07:29:26 +0300

    Beginning remake branch

---
+++ Header Files/defs.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef struct Point
+{
+    int x, y;
+} Point;
+
+#define LONG 109
+#define LARG 109
---
+++ Source Files/main.cpp
@@ -0,0 +1,10 @@
+#include <SDL3/SDL_image.h>
+#include <SDL3/SDL.h>
+
+#include "maze.h"
+
+int main(int argc, char *argv[])
+{
+    Maze().run();
+    return 0;
+}
